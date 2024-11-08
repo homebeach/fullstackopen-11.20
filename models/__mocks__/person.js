da32fd3f701a896e7fa580757975e103f8a18ca4
Petri Kotiranta <petri.kotiranta@exaum.com>
2024-11-08 23:40:15 +0200

    11.20 fix

--- models/__mocks__/person.js
+++ models/__mocks__/person.js
@@ -3,26 +3,22 @@ const mockData = [
   { id: '2', content: 'Bob', number: '789-012' },
 ]
 
-// Constructor function to simulate the creation of a new Person instance
 class Person {
   constructor(data) {
     this.content = data.content
     this.number = data.number
-    this.id = data.id || '123' // Default id if not provided
+    this.id = data.id || '123'
 
-    // Mock save method
     this.save = jest.fn().mockResolvedValue({
       content: this.content,
       number: this.number,
       id: this.id,
     })
 
-    // Mock validate method (resolve as valid for testing purposes)
     this.validate = jest.fn().mockResolvedValue(true)
   }
 }
 
-// Static methods for Person (mimicking Mongoose model)
 Person.find = jest.fn().mockResolvedValue(mockData)
 Person.findById = jest.fn((id) => Promise.resolve(mockData.find(person => person.id === id)))
 Person.findByIdAndRemove = jest.fn().mockResolvedValue(null)
@@ -32,5 +28,4 @@ Person.create = jest.fn((person) => Promise.resolve(new Person(person)))
 
 const mockPersonConstructor = jest.fn((data) => new Person(data))
 
-// Export the mock class as default
 export default mockPersonConstructor
